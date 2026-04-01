28eba0df48e82468a302821cbfc894af1becbf40
nikhil-thorat <nikhilthoratofficial@gmail.com>
2026-04-02 00:57:45 +0530

    Added new HashFunc and refactoring

--- src/map.c
+++ src/map.c
@@ -5,18 +5,76 @@
 #include <string.h>
 
 /*
- * FNV-1a Hashing algorithm
+ * MurmurHash3 (32-bit)
+ * Extremely fast, excellent collision resistance.
+ * Processes 4 bytes at a time instead of 1 byte.
  */
 static uint32_t HashString(const char *key)
 {
-    uint32_t hash = 2166136261u;
-    for (int i = 0; key[i] != '\0'; i++)
+    int len = strlen(key);
+    const uint8_t *data = (const uint8_t *)key;
+
+    uint32_t h1 = 0x9747b28c;
+
+    const uint32_t c1 = 0xcc9e2d51;
+    const uint32_t c2 = 0x1b873593;
+
+    const int nblocks = len / 4;
+    const uint32_t *blocks = (const uint32_t *)(data);
+
+    for (int i = 0; i < nblocks; i++)
     {
-        hash ^= (uint8_t)key[i];
-        hash *= 16777619;
+        uint32_t k1 = blocks[i];
+
+        k1 *= c1;
+        k1 = (k1 << 15) | (k1 >> (32 - 15));
+        k1 *= c2;
+
+        h1 ^= k1;
+        h1 = (h1 << 13) | (h1 >> (32 - 13));
+        h1 = h1 * 5 + 0xe6546b64;
     }
-    return hash;
+
+    const uint8_t *tail = (const uint8_t *)(data + nblocks * 4);
+    uint32_t k1 = 0;
+
+    switch (len & 3)
+    {
+    case 3:
+        k1 ^= tail[2] << 16;
+    case 2:
+        k1 ^= tail[1] << 8;
+    case 1:
+        k1 ^= tail[0];
+        k1 *= c1;
+        k1 = (k1 << 15) | (k1 >> (32 - 15));
+        k1 *= c2;
+        h1 ^= k1;
+    }
+
+    h1 ^= len;
+    h1 ^= h1 >> 16;
+    h1 *= 0x85ebca6b;
+    h1 ^= h1 >> 13;
+    h1 *= 0xc2b2ae35;
+    h1 ^= h1 >> 16;
+
+    return h1;
 }
+
+/*
+ * FNV-1a Hashing algorithm
+ */
+// static uint32_t HashString(const char *key)
+// {
+//     uint32_t hash = 2166136261u;
+//     for (int i = 0; key[i] != '\0'; i++)
+//     {
+//         hash ^= (uint8_t)key[i];
+//         hash *= 16777619;
+//     }
+//     return hash;
+// }
 
 Map *NewMap(size_t capacity)
 {
@@ -81,9 +139,17 @@ int MapSet(Map *map, const char *key, Node *node)
             Node *existing_node = slot->node;
             if (strcmp(existing_node->key, key) == 0)
             {
-                UpdateNodeValue(existing_node, node->value);
-                FreeNode(node);
-                return 0;
+                int result = UpdateNodeValue(existing_node, node->value);
+                if (result == 1)
+                {
+                    FreeNode(node);
+                    return 0;
+                }
+                else
+                {
+                    FreeNode(node);
+                    return -1;
+                }
             }
         }
 
