ada0c8ef32da759bfe9818e7d31d60b18a2da970
nikhil-thorat <nikhilthoratofficial@gmail.com>
2026-04-02 20:56:21 +0530

    Added database header and implementation files

---
+++ include/database.h
@@ -0,0 +1,46 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include "shard.h"
+
+/*
+ * Represents actual database engine, contains
+ * multiple shards for maximum performance.
+ */
+typedef struct Database
+{
+    Shard **shards;
+    size_t num_shards;
+} Database;
+
+/*
+ * Initializes a new Database with specified no of shards.
+ * Returns a pointer to the created Database.
+ */
+Database *NewDatabase(size_t num_shards, size_t shard_capacity);
+
+/*
+ * Set's the given key and value in the Datbase.
+ * Returns 1 if successful, else 0.
+ */
+int DatabaseSet(Database *db, const char *key, const char *value);
+
+/*
+ * Return's the pointer to the value of the given
+ * key if it exists in the Database.
+ */
+char *DatabaseGet(Database *db, const char *key);
+
+/*
+ * Delete's the key-value from the Database if it exits
+ * in the Database. Returns 1 if deleted, else 0.
+ */
+int DatabaseDel(Database *db, const char *key);
+
+/*
+ * Delete's everything in the Database and free's the memory
+ * occupied by it.
+ */
+void DestroyDatabase(Database *db);
+
+#endif
---
+++ src/database.c
@@ -0,0 +1,95 @@
+#include "../include/database.h"
+#include "../include/hash.h"
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+Database *NewDatabase(size_t num_shards, size_t shard_capacity)
+{
+    Database *db = (Database *)malloc(sizeof(Database));
+    if (db == NULL)
+    {
+        return NULL;
+    }
+
+    db->num_shards = num_shards;
+
+    db->shards = (Shard **)malloc(sizeof(Shard *) * num_shards);
+    if (db->shards == NULL)
+    {
+        free(db);
+        return NULL;
+    }
+
+    for (size_t i = 0; i < num_shards; i++)
+    {
+        db->shards[i] = NewShard(shard_capacity);
+        if (db->shards[i] == NULL)
+        {
+            for (size_t j = 0; j < i; j++)
+            {
+                free(db->shards);
+                free(db);
+                return NULL;
+            }
+        }
+    }
+
+    return db;
+}
+
+int DatabaseSet(Database *db, const char *key, const char *value)
+{
+    if (db == NULL || key == NULL || value == NULL)
+    {
+        return 0;
+    }
+
+    uint32_t hash = Hash(key);
+    size_t shard_index = hash % (db->num_shards - 1);
+
+    return ShardSet(db->shards[shard_index], key, value);
+};
+
+char *DatabaseGet(Database *db, const char *key)
+{
+
+    if (db == NULL || key == NULL)
+    {
+        return 0;
+    }
+
+    uint32_t hash = Hash(key);
+    size_t shard_index = hash % (db->num_shards - 1);
+
+    return ShardGet(db->shards[shard_index], key);
+};
+
+int DatabaseDelete(Database *db, const char *key)
+{
+    if (db == NULL || key == NULL)
+    {
+        return 0;
+    }
+
+    uint32_t hash = Hash(key);
+    size_t shard_index = hash % (db->num_shards - 1);
+
+    return ShardDelete(db->shards[shard_index], key);
+};
+
+void DestroyDatabase(Database *db)
+{
+    if (db == NULL)
+    {
+        return;
+    }
+
+    for (size_t i = 0; i < db->num_shards; i++)
+    {
+        DestroyShard(db->shards[i]);
+    }
+
+    free(db->shards);
+    free(db);
+}
